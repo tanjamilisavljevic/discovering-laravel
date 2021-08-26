146a68e42ae09f3a9ffb3c00d5b6277507733f91
Tanja <tanjamilisavljevic7@gmail.com>
2021-08-26 10:17:59 +0200

    Link bootstrap

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -6,15 +6,15 @@
 <form method="POST" action="register">
     @csrf
 
-    <label for="name">Name:</label>
+    <label for="name" class="col-form-label">Name:</label>
     <input type="text" name="name" id="name">
 
-    <label for="course">Course name:</label>
+    <label for="course" class="col-form-label">Course name:</label>
     <input type="text" name="course" id="course">
 
-    <label for="email">E-mail:</label>
+    <label for="email" class="col-form-label">E-mail:</label>
     <input type="email" id="email" name="email">
 
-    <input type="submit" id="submit" name="submit" value="Go">
+    <input type="submit" id="submit" name="submit" class="btn btn-primary" value="Go">
 </form>
 @endsection
