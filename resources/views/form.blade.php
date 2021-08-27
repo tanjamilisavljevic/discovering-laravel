a3055cbc42067f96f04317b39d510a821d662b0c
Tanja <tanjamilisavljevic7@gmail.com>
2021-08-27 10:49:13 +0200

    Add styling for the form

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,20 +1,26 @@
-
 @extends('layout')
 @extends('header')
 @extends('footer')
 @section('container')
-<form method="POST" action="register">
-    @csrf
+    <form method="POST" action="register">
+        @csrf
+        <div class="form">
+            <div class="formWrapper">
+                <label for="name" class="label">Name:</label>
+                <input type="text" name="name" id="name" class="input">
 
-    <label for="name">Name:</label>
-    <input type="text" name="name" id="name">
+                <label for="course" class="label">Course name:</label>
+                <input type="text" name="course" id="course" class="input">
 
-    <label for="course">Course name:</label>
-    <input type="text" name="course" id="course">
+                <label for="email" class="label">E-mail:</label>
+                <input type="email" id="email" name="email" class="input">
+            </div>
+        </div>
+        <div class="buttonWrapper">
+            <input class="submitButton" type="submit" id="submit" name="submit" value="Go">
+            <a href="../public/" class="link" id="homeLink">🌾 Go home</a>
+        </div>
+    </form>
 
-    <label for="email">E-mail:</label>
-    <input type="email" id="email" name="email">
 
-    <input type="submit" id="submit" name="submit" value="Go">
-</form>
 @endsection
