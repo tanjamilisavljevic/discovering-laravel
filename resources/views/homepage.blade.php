07031a64089a3e3056aad628e0a6e442c47febe9
Tanja <tanjamilisavljevic7@gmail.com>
2021-08-27 12:21:58 +0200

    Add styling for button on the form

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,7 +7,6 @@
         <a href="form" class="link">🤠 To register form</a>
         <a href="users" class="link">🐎 To the users' names</a>
         <a href="selected-users" class="link">👢 To the users' names in Hi course</a>
-
     </div>
     <div class="cowWrapper">
         <img src="../public/cow.png" class="cow">
